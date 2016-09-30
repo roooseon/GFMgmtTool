96408584cdbe8f94bed8fc8cc8b4ef32f34899b7
RojanPiya <piya_roj@hotmail.com>
2016-09-30 16:44:09 -0500

    adds base project setup

---
+++ src/mainWindow.java
@@ -0,0 +1,126 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import javax.swing.JButton;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JPasswordField;
+
+public class mainWindow {
+
+	private JFrame frame;
+	private JTextField txtUsername;
+	private JButton btnLogin;
+	private JButton btnRegister;
+	private JPasswordField pwdPassword;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					mainWindow window = new mainWindow();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public mainWindow() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{428, 0};
+		gridBagLayout.rowHeights = new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.insets = new Insets(0, 0, 5, 0);
+		gbc_panel_1.anchor = GridBagConstraints.NORTH;
+		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 0;
+		frame.getContentPane().add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{0};
+		gbl_panel_1.rowHeights = new int[]{0};
+		gbl_panel_1.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		JLabel lblGfMgmt = new JLabel("GF MGMT");
+		GridBagConstraints gbc_lblGfMgmt = new GridBagConstraints();
+		gbc_lblGfMgmt.insets = new Insets(20, 0, 10, 0);
+		gbc_lblGfMgmt.gridx = 0;
+		gbc_lblGfMgmt.gridy = 1;
+		frame.getContentPane().add(lblGfMgmt, gbc_lblGfMgmt);
+		
+		txtUsername = new JTextField();
+		txtUsername.setToolTipText("Username");
+		GridBagConstraints gbc_txtUsername = new GridBagConstraints();
+		gbc_txtUsername.ipady = 10;
+		gbc_txtUsername.insets = new Insets(10, 25, 10, 25);
+		gbc_txtUsername.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtUsername.gridx = 0;
+		gbc_txtUsername.gridy = 2;
+		frame.getContentPane().add(txtUsername, gbc_txtUsername);
+		txtUsername.setColumns(10);
+		
+		pwdPassword = new JPasswordField();
+		pwdPassword.setToolTipText("Password");
+		GridBagConstraints gbc_pwdPassword = new GridBagConstraints();
+		gbc_pwdPassword.ipady = 10;
+		gbc_pwdPassword.insets = new Insets(10, 25, 10, 25);
+		gbc_pwdPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_pwdPassword.gridx = 0;
+		gbc_pwdPassword.gridy = 3;
+		frame.getContentPane().add(pwdPassword, gbc_pwdPassword);
+		
+		btnLogin = new JButton("Login");
+		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
+		gbc_btnLogin.insets = new Insets(15, 0, 10, 0);
+		gbc_btnLogin.fill = GridBagConstraints.VERTICAL;
+		gbc_btnLogin.gridx = 0;
+		gbc_btnLogin.gridy = 4;
+		frame.getContentPane().add(btnLogin, gbc_btnLogin);
+		
+		btnRegister = new JButton("Register");
+		btnRegister.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		GridBagConstraints gbc_btnRegister = new GridBagConstraints();
+		gbc_btnRegister.insets = new Insets(10, 0, 10, 0);
+		gbc_btnRegister.gridx = 0;
+		gbc_btnRegister.gridy = 5;
+		frame.getContentPane().add(btnRegister, gbc_btnRegister);
+	}
+
+}
